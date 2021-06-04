75e4131a26115a2c968c28cb1cc877f131656fcf
guyush1 <guy.shimko@gmail.com>
2021-06-04 13:07:29 +0300

    Db revamped: now using a singleton instance for handling the db

--- db/db_handler.py
+++ db/db_handler.py
@@ -2,70 +2,83 @@ import db.db_queries as db_queries
 import sqlite3
 from typing import Union
 
+class DBSingleton(type):
+    def __init__(self, name, bases, dic):
+        self.__single_instance = None
+        super().__init__(name, bases, dic)
+ 
+    def __call__(cls, *args, **kwargs):
+        if cls.__single_instance:
+            return cls.__single_instance
 
-def initialize_db():
+        single_obj = cls.__new__(cls)
+        single_obj.__init__(*args, **kwargs)
+        cls.__single_instance = single_obj
+        return single_obj
+
+class DBHandler(metaclass=DBSingleton):
     """
-    function creates the required db file and tables if needed.
+    Class responsible handling database related operations.
+    This class is defined as a singleton to maintain one instance of db handling.
     """
-    print("DB initializing\n")
 
-    # Create a global variable for accessing the db
-    global db_con
+    def __init__(self):
+        """
+        function creates the required db file and tables if needed.
+        """
+        print("DB initializing\n")
 
-    # Create the db file
-    db_con = sqlite3.connect(db_queries.DB_NAME)
+        # Create the db file
+        self.db_con = sqlite3.connect(db_queries.DB_NAME)
 
-    # Create the tables if they don't exist
-    create_table_if_needed(db_queries.SQL_CREATE_SONGS_TABLE)
-    create_table_if_needed(db_queries.SQL_CREATE_ALBUMS_TABLE)
+        # Create the tables if they don't exist
+        self.create_table_if_needed()
 
+    def create_table_if_needed(self):
+        """
+        Creates a new sql table from a sql_query if it doesnt exist
 
-def create_table_if_needed(sql_query):
-    """
-    Creates a new sql table from a sql_query if it doesnt exist
+        :param con: Our connection to the db
+        :param sql_query: the query to creating the sqlLite table
+        """
+        try:
+            c = self.db_con.cursor()
+            c.execute(db_queries.SQL_CREATE_SONGS_TABLE)
+            c.execute(db_queries.SQL_CREATE_ALBUMS_TABLE)
+        except sqlite3.Error as err:
+            print(err)
 
-    :param con: Our connection to the db
-    :param sql_query: the query to creating the sqlLite table
-    """
-    try:
-        c = db_con.cursor()
-        c.execute(sql_query)
-    except sqlite3.Error as err:
-        print(err)
+    def add_song(self, song_name: str, artist_name: str, song_album_name: Union[str, None], is_private: bool):
+        """
+        Adds a song to the db.
 
+        :param db_con: The connection to the db
+        :param song_name: The name of the song we are adding
+        :param song_album_name: The name of the album the song is related to, None if it is a single
+        :param is_private: indicated whether the song is public or private
+        """
+        try:
+            c = self.db_con.cursor()
+            c.execute(db_queries.SQL_ADD_SONG,
+                    # Parameters as a tuple
+                    (song_name, artist_name, "" if not song_album_name else song_album_name, int(is_private), int(False)))
+            self.db_con.commit()
+        except sqlite3.Error as err:
+            print(err)
 
-def get_song_name(song_name: str, song_album_name: Union[str, None], is_private: bool):
-    """
-    Adds a song to the db.
+    def add_album(self, album_name: str, artist_name: str, is_private: bool):
+        """
+        Adds an album to the db.
 
-    :param db_con: The connection to the db
-    :param song_name: The name of the song we are adding
-    :param song_album_name: The name of the album the song is related to, None if it is a single
-    :param is_private: indicated whether the song is public or private
-    """
-    try:
-        c = db_con.cursor()
-        c.execute(db_queries.SQL_ADD_SONG,
-                  # Parameters as a tuple
-                  (song_name, "" if not song_album_name else song_album_name, int(is_private), int(False)))
-        db_con.commit()
-    except sqlite3.Error as err:
-        print(err)
-
-
-def add_album(album_name: str, is_private: bool):
-    """
-    Adds an album to the db.
-
-    :param db_con: The connection to the db
-    :param album_name: The name of the album we are adding
-    :param is_private:  indicated whether the song is public or private
-    """
-    try:
-        c = db_con.cursor()
-        c.execute(db_queries.SQL_ADD_ALBUM,
-                  # Parameters as a tuple
-                  (album_name, int(is_private), int(False)))
-        db_con.commit()
-    except sqlite3.Error as err:
-        print(err)
+        :param db_con: The connection to the db
+        :param album_name: The name of the album we are adding
+        :param is_private:  indicated whether the song is public or private
+        """
+        try:
+            c = self.db_con.cursor()
+            c.execute(db_queries.SQL_ADD_ALBUM,
+                    # Parameters as a tuple
+                    (album_name, artist_name, int(is_private), int(False)))
+            self.db_con.commit()
+        except sqlite3.Error as err:
+            print(err)
--- db/db_queries.py
+++ db/db_queries.py
@@ -1,21 +1,25 @@
 DB_NAME = "db/musicDB.db"
 
 SQL_CREATE_SONGS_TABLE = """ CREATE TABLE IF NOT EXISTS songs (
-                                song_name text PRIMARY KEY,
+                                artist_name text,
+                                song_name text,
                                 is_private int,
                                 album_name text,
-                                added_to_sd int
+                                added_to_sd int,
+                                PRIMARY KEY (artist_name, song_name)
                             ); """
 
 SQL_CREATE_ALBUMS_TABLE = """ CREATE TABLE IF NOT EXISTS albums (
-                                album_name text PRIMARY KEY,
+                                artist_name text ,
+                                album_name text,
                                 is_private int,
-                                added_to_sd int
+                                added_to_sd int,
+                                PRIMARY KEY (artist_name, album_name)
                             ); """
 
-SQL_ADD_SONG = """ INSERT INTO songs (song_name, album_name, is_private, added_to_sd)
-                   VALUES(?,?,?,?) """
+SQL_ADD_SONG = """ INSERT INTO songs (song_name, artist_name, album_name, is_private, added_to_sd)
+                   VALUES(?,?,?,?,?) """
 
-SQL_ADD_ALBUM = """ INSERT INTO albums (album_name, is_private, added_to_sd)
-                    VALUES(?,?,?) """
+SQL_ADD_ALBUM = """ INSERT INTO albums (album_name, artist_name, is_private, added_to_sd)
+                    VALUES(?,?,?,?) """
 
